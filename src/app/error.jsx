68345c59fed3d63f690356d1053f47e5b134f7d3
Kartavya Jain <kartavya2552@gmail.com>
2024-06-19 16:13:32 +0530

    fixed error

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect } from 'react'
 
-export default function Error({ error, reset }) {
+export default function ErrorComponent({ error, reset }) {
   useEffect(() => console.log(error), [error]);
   return (
     <div className='text-center mt-10'>
